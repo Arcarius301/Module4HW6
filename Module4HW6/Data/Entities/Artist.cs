45c755c57d109ca6046ca510ca96a8f8e6faa84b
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-01-30 13:47:20 +0200

    Add entities.

---
+++ Module4HW6/Data/Entities/Artist.cs
@@ -0,0 +1,13 @@
+﻿namespace Module4HW6.Data.Entities
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? InstagramUrl { get; set; }
+        public List<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ Module4HW6/Data/Entities/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace Module4HW6.Data.Entities
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Duration { get; set; } = null!;
+        public DateOnly ReleasedDate { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; } = null!;
+        public List<Artist> Artists { get; set; } = new List<Artist>();
+    }
+}
