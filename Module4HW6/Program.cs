38ee8ac1aa6e89d5fc5a5dcfbc54f40c7d8830de
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-01-30 13:03:44 +0200

    Add project files.

---
+++ Module4HW6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module4HW6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
