a8effa0fd93cc715399d270ddb566d573e1f8152
Pavel Kaznachei <pavel.kaznachei@gmail.com>
2022-01-30 16:57:23 +0200

    Add trycatch transaction wrapper.

---
+++ Module4HW6/Helpers/QueryWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Module4HW6.Data;
+using Module4HW6.Helpers;
+
+namespace Module4HW6.Helpers
+{
+    public class QueryWrapper
+    {
+        public static async Task Run(Func<Task> function, string[] args)
+        {
+            await using (var transaction = await new ContextFactory().CreateDbContext(args).Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await function();
+                    await transaction.CommitAsync();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    await transaction.RollbackAsync();
+                }
+            }
+        }
+    }
+}
